973e4c1cb388cc1b97bcf4b656d9318fa23d72f6
Gilbert_CEY <52315288+ceygilbert@users.noreply.github.com>
2024-03-24 01:55:58 +0800

    Update Worker.js
    testing

--- components/Worker.js
+++ components/Worker.js
@@ -5,11 +5,11 @@ addEventListener("message", async ({ data }) => {
     const input = tf.tensor(JSON.parse(data));
     const proxyUrl = 'https://greenfy-alpha.vercel.app/api/proxy?url=' + encodeURIComponent(process.env.NEXT_PUBLIC_MODEL_URL);
     const temp = await tf.loadGraphModel(process.env.NEXT_PUBLIC_MODEL_URL);
-    console.log("1":, temp);
+    console.log('1':, temp);
     const response = await fetch(proxyUrl);
-    console.log("2:", response);
+    console.log('2:', response);
     const modelData = await response.json();
-    console.log("3:", modelData);
+    console.log('3:', modelData);
 
     const Model = await tf.loadGraphModel(modelData);
     const result = await Model.predict(tf.expandDims(input, 0));
