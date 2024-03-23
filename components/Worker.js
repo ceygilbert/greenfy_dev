9ae6374a5ef455263140da7df63ebde71a31587c
Gilbert_CEY <52315288+ceygilbert@users.noreply.github.com>
2024-03-24 02:44:45 +0800

    Update Worker.js
    debug

--- components/Worker.js
+++ components/Worker.js
@@ -1,43 +1,20 @@
+// worker.js
+
 import * as tf from "@tensorflow/tfjs";
 
 addEventListener("message", async ({ data }) => {
-  try {
-    const input = tf.tensor(JSON.parse(data));
-    const proxyUrl = 'https://greenfy-alpha.vercel.app/api/proxy?url=' + encodeURIComponent(process.env.NEXT_PUBLIC_MODEL_URL);
-    const response = await fetch(proxyUrl);
-    const modelData = await response.json();
+  let Model;
+  const input = tf.tensor(data);
 
-    const blob = new Blob([JSON.stringify(modelData)], { type: 'application/json' });
-
-    const url = URL.createObjectURL(blob);
-    const Model = await tf.loadGraphModel(url);
-
-    const result = await Model.predict(tf.expandDims(input, 0));
-    const index = await result.data();
-
-    URL.revokeObjectURL(url);
-
-    const final = {
-      number: indexOfMax(index) + 1,
-      tensor: JSON.stringify(input.arraySync()),
-    };
-
-    postMessage(final);
-  } catch (error) {
-    console.error('Error:', error);
-  }
-});
-
-function indexOfMax(arr) {
-  // indexOfMax function implementation remains unchanged
-  if (arr.length === 0) {
+  function indexOfMax(arr) {
+    if (arr.length === 0) {
       return -1;
     }
 
-    var max = arr[0];
-    var maxIndex = 0;
+    let max = arr[0];
+    let maxIndex = 0;
 
-    for (var i = 1; i < arr.length; i++) {
+    for (let i = 1; i < arr.length; i++) {
       if (arr[i] > max) {
         maxIndex = i;
         max = arr[i];
@@ -45,7 +22,30 @@ function indexOfMax(arr) {
     }
 
     return maxIndex;
-}
+  }
+
+  if (!Model) {
+    // Use the API route URL to fetch the model JSON file
+    const proxyUrl = '/api/proxy?url=' + encodeURIComponent(process.env.NEXT_PUBLIC_MODEL_URL);
+    try {
+      // Load the TensorFlow.js model from the API route URL
+      Model = await tf.loadGraphModel(proxyUrl);
+    } catch (error) {
+      console.error('Error loading TensorFlow.js model:', error);
+      return;
+    }
+  }
+  
+  const result = await Model.predict(tf.expandDims(input, 0));
+  const index = await result.data();
+
+  const final = {
+    number: indexOfMax(index) + 1,
+    tensor: JSON.stringify(input.arraySync()),
+  };
+
+  postMessage(final);
+});
 
 
 
