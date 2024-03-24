3f3a2e8da18888f384736475999bdb3957013839
Gilbert_CEY <52315288+ceygilbert@users.noreply.github.com>
2024-03-24 10:22:43 +0800

    Update Worker.js

--- components/Worker.js
+++ components/Worker.js
@@ -4,12 +4,12 @@ import * as tf from "@tensorflow/tfjs";
 
 addEventListener("message", async ({ data }) => {
   let Model;
-  //const input = tf.tensor(JSON.parse(data));
+  const input = tf.tensor(JSON.parse(data));
   
-  const parsedData = JSON.parse(data);
+  //const parsedData = JSON.parse(data);
 
   // Convert the parsed data into a TensorFlow tensor
-  const input = tf.tensor(parsedData);
+  //const input = tf.tensor(parsedData);
 
   function indexOfMax(arr) {
     if (arr.length === 0) {
@@ -31,9 +31,9 @@ addEventListener("message", async ({ data }) => {
 
   if (!Model) {
     console.log(tf);
-    const proxyUrl = '/api/proxy?url=' + encodeURIComponent(process.env.NEXT_PUBLIC_MODEL_URL);
+    //const proxyUrl = '/api/proxy?url=' + encodeURIComponent(process.env.NEXT_PUBLIC_MODEL_URL);
     // Make sure to update the NEXT_PUBLIC_MODEL_URL environment variable on Vercel (or .env file) to point to the model.json file
-    Model = await tf.loadGraphModel(proxyUrl);
+    Model = await tf.loadGraphModel(process.env.NEXT_PUBLIC_MODEL_URL);
   }
   const result = await Model.predict(tf.expandDims(input, 0));
   const index = await result.data();
